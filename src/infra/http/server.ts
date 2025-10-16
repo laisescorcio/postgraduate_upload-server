ccb21c079ee9590d0f3180429365fb3acff50cea
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2025-10-16 07:45:18 -0300

    setup .env

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,3 +1,4 @@
+import { env } from "@/env";
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
 
@@ -6,6 +7,8 @@ const server = fastify();
 server.register(fastifyCors, {
   origin: "*",
 });
+
+console.log(env.DATABASE_URL);
 
 server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log(`HTTP server running`);
